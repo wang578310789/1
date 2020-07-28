0ee5a54f2532871eebffa0c64ecb40e3718b004f
578310789 <578310789@qq.com>
2020-07-28 10:18:46 +0800

    7.28暂时提交

---
+++ src/components/goods/List.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容"
+            v-model="queryInfo.query" clearable
+            @clear="getGoodsList">
+            <el-button slot="append"
+              icon="el-icon-search"
+              @click="getGoodsList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="goAddpage">添加商品</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- table表格区域 -->
+      <el-table :data="goodsList" border stripe>
+        <el-table-column type="index" />
+        <el-table-column label="商品名称" prop="goods_name" />
+        <el-table-column label="商品价格(元)" prop="goods_price" width="95px" />
+        <el-table-column label="商品重量" prop="goods_weight" width="95px" />
+        <el-table-column label="创建时间" prop="add_time" width="160px">
+          <template slot-scope="scope">
+            {{ scope.row.add_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini"
+              icon="el-icon-edit"></el-button>
+            <el-button type="danger" size="mini"
+              icon="el-icon-delete"
+              @click="removeById(scope.row.goods_id)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分类区域 -->
+      <el-pagination @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]" background
+        :page-size="queryInfo.pagesize" :total="total"
+        layout="total, sizes, prev, pager, next, jumper">
+      </el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      total: 0,
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 商品列表
+      goodsList: []
+    }
+  },
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    // 根据分页获取对应的商品列表
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+
+      if (res.meta.status == 200) {
+        this.$message.success('获取商品列表成功！')
+        this.goodsList = res.data.goods
+        this.total = res.data.total
+      } else {
+        this.$message.error('获取商品列表失败！');
+      }
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    },
+    async removeById(id) {
+      const confirmResult = await this.$confirm('此操作将永远删除该文件, 是否继续？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已经取消删除！')
+      }
+
+      const { data: res } = await this.$http.delete(`goods/${id}`)
+      if (res.meta.status == 200) {
+        this.getGoodsList()
+        this.$message.success('删除成功！')
+      } else {
+        this.$message.error('删除失败！')
+      }
+    },
+    goAddpage() {
+      this.$router.push('/goods/add')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementUI from 'element-ui';
 import axios from 'axios'
 import Qs from 'qs'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
 
 import router from './router'
 import store from './store'
@@ -11,6 +12,9 @@ import store from './store'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import 'element-ui/lib/theme-chalk/index.css';
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$qs = Qs
@@ -24,8 +28,24 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.use(ElementUI);
+Vue.use(VueQuillEditor);
 
 Vue.component('tree-table', TreeTable)
+
+// 自定义格式化 时间格式
+Vue.filter('dateFormat', originVal => {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
